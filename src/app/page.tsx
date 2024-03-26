3d403e67e48fd2418fee6ac66c8d2254e4630453
huynguyeexn <huynguyeexn@gmail.com>
2024-03-26 07:54:53 +0000

    Disable scroll area mobile, add scroll top btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
   );
 
   return (
-    <main className="container text-sky-950 pb-8">
+    <main className="p-4 sm:container text-sky-950 pb-8 mt-4">
       <article className={articleStyle}>
         <section className="bg-sky-50/50 sm:bg-transparent backdrop-blur-[2px] sm:backdrop-blur-none p-5 rounded-3xl">
           <div className="w-full md:w-1/2">
             <HeroComponent />
           </div>
-          <div className="form-wrapper mt-20">
+          <div className="form-wrapper mt-10 sm:mt-20">
             <SearchBoxComponent />
           </div>
         </section>
